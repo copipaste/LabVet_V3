503fe69880e244d5688a7f9fec41df01a67746ff
copipaste <jhoeldebrayquispe@gmail.com>
2024-11-05 21:10:47 -0400

    creacion de proyecto

---
+++ src/main/java/com/mycompany/labvet_v3/LabVet_V3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.labvet_v3;
+
+/**
+ *
+ * @author Usuario
+ */
+public class LabVet_V3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
